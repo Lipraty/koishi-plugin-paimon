1ae0ddf1a2bdb10cd272700b11fe38fadd2fbca6
Lipraty <sis1943@outlook.com>
2022-09-25 19:08:09 +0800

    refa(genshin/core): `Hoyo` tool rewrite

--- src/genshin/core/utils/Hoyo.ts
+++ src/genshin/core/utils/Hoyo.ts
@@ -1,34 +1,122 @@
-import { HoyoDS } from "./DS"
+import { createHash } from "node:crypto"
 import { Random } from "./Random"
 import { ServerType } from "./ServerType"
+import { UUID } from "./UUID"
+
+type ClientType = 1 | 2 | 3 | 4 | 5
+
+const config = {
+    act_id: 'e202009291139501',
+    saltCN: 'xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs',
+    saltOS: 'n0KjuIrKgLHh08LWSCYP0WXlVXaYvV64',
+    appverCN: '2.36.1',
+    appverOS: '2.9.0',
+    headerCN: 'miHoYoBBS',
+    headerOS: 'miHoYoBBSOversea'
+}
 
 export class Hoyo {
-    public static readonly act_id = 'e202009291139501'
+    public readonly act_id = config.act_id
+    private salt: string = 'YVEIkzDFNHLeKXLxzqCA9TzxCpWwbIbk'
+    private appVersion: string
+    private clientType: ClientType
+    private st: 'cn' | 'os'
+    private _UAEnd: string
+    private device: string
 
-    public static headers(serverType: ServerType, device: string, cookie?: string, query?: Record<string, any>, param?: Record<string, any>): HeadersInit {
-        const _device = device || `Paimon/${Random.randstr(12)}`
-        if (cookie)
-            return {
-                'x-rpc-app_version': HoyoDS.appVersion,
-                'x-rpc-client_type': HoyoDS.clientType,
-                'x-rpc-device_id': device,
-                'User-Agent': Random.randUA(HoyoDS.appVersion, _device),
-                'X-Requested-With': 'com.mihoyo.hyperion',
-                'x-rpc-platform': 'android',
-                'x-rpc-device_model': `MI ${_device}`,
-                'x-rpc-device_name': _device,
-                'x-rpc-channel': 'miyousheluodi',
-                'x-rpc-sys_version': '6.0.1',
-                'Referer': Random.randRef(Hoyo.act_id),
-                'DS': new HoyoDS(serverType).getSign(),
-                'cookie': cookie
-            }
-        else
-            return {
-                'x-rpc-app_version': HoyoDS.appVersion,
-                'x-rpc-client_type': HoyoDS.clientType,
-                'User-Agent': Random.randUA(HoyoDS.appVersion, _device),
-                'DS': new HoyoDS(serverType).get2Android(query, param)
-            }
+    public static readonly act_id: string = config.act_id
+
+    constructor(serverType: ServerType, device?: string)
+    constructor(serverType: ServerType, client?: ClientType)
+    constructor(serverType: ServerType, deviceORclient?: string | ClientType) {
+        if (serverType === ServerType.CN || serverType === ServerType.CNB) {
+            this.st = 'cn'
+            this.salt = config.saltCN
+            this.appVersion = config.appverCN
+            this.clientType = 5
+            this._UAEnd = `miHoYoBBS/${this.appVersion}`
+        } else {
+            this.st = 'os'
+            this.salt = config.saltOS
+            this.appVersion = config.appverOS
+            this.clientType = 2
+            this._UAEnd = `miHoYoBBSOversea/${this.appVersion}`
+        }
+
+        if (typeof deviceORclient === 'string') {
+            this.device = deviceORclient
+        } else {
+            this.device = this.createDevice()
+            this.clientType = deviceORclient
+        }
+    }
+
+    //# miHoyoDS
+    /**
+     * 获取新版ds内容
+     * @param body 
+     * @param params 
+     * @returns 
+     */
+    public newDS(body?: Record<string, any>, params?: Record<string, any>): string {
+        let t: string = Math.round(new Date().getTime() / 1000).toString()
+        let r: string = Random.randint(100001, 200000).toString()
+        let b: string = body ? JSON.stringify(body) : ''
+        let q: string = params ? encodeURI(JSON.stringify(params)) : ''
+        let c: string = this.hash({ t, r, b, q })
+        return `${t},${r},${c}`
+    }
+
+    /**
+     * 旧版本ds内容，主要用于签到
+     */
+    public oldDS(): string {
+        let t: string = Math.round(new Date().getTime() / 1000).toString()
+        let r: string = Random.sample('abcdefghijklmnopqrstuvwxyz0123456789'.split(''), 6).join('')
+        let c: string = this.hash({ t, r })
+        return `${t},${r},${c}`
+    }
+
+    private hash(value: Record<string, any>): string {
+        //默认加入salt
+        let temp: string[] = ['salt=' + this.salt];
+        //将object拆分为键值对array
+        Object.keys(value).forEach(key => {
+            temp.push(`${key}=${value[key]}`)
+        })
+        //序列化并md5哈希
+        return createHash('md5').update(temp.join('&')).digest('hex')
+    }
+    //# miHoyoDS END
+
+    public createDevice() {
+        return `Paimon/${Random.randstr(12)}`
+    }
+
+    public headers(query?: Record<string, any>, body?: Record<string, any>): HeadersInit {
+        return {
+            'x-rpc-app_version': this.appVersion,
+            'x-rpc-client_type': this.clientType.toString(),
+            'User-Agent': [Random.randUA(this.device), this._UAEnd].join(' '),
+            'Referer': this.st === 'cn' ? `https://webstatic.mihoyo.com/bbs/event/signin-ys/index.html?bbs_auth_required=true&act_id=${this.act_id}&utm_source=bbs&utm_medium=mys&utm_campaign=icon` : `https://webstatic-sea.hoyolab.com`,
+            'DS': this.newDS(query, body)
+        }
+    }
+
+    public signHeader() {
+        this.salt = 'YVEIkzDFNHLeKXLxzqCA9TzxCpWwbIbk'
+        let headers = this.headers()
+
+        headers['DS'] = this.oldDS()
+
+        return Object.assign(headers, {
+            'x-rpc-device_id': UUID.randomUUID(),
+            'x-rpc-platform': 'android',
+            'x-rpc-device_model': `MI ${this.device}`,
+            'x-rpc-device_name': `MI ${this.device}`,
+            'x-rpc-channel': 'miyousheluodi',
+            'x-rpc-sys_version': '6.0.1',
+            'X-Requested-With': this.st === 'cn' ? 'com.mihoyo.hyperion' : 'com.mihoyo.hoyolab'
+        })
     }
 }
--- src/genshin/core/utils/Random.ts
+++ src/genshin/core/utils/Random.ts
@@ -32,21 +32,13 @@ export class Random {
         return str
     }
 
-    public static randUA(appVer: string, device): string {
+    public static randUA(device: string): string {
         let UAS = [
             `Mozilla/5.0 (Linux; Android 12; ${device}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36`,
-            `Mozilla/5.0 (Linux; Android 12; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36`
+            `Mozilla/5.0 (Linux; Android 12; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36`,
+            `Mozilla/5.0 (Linux; Android 11; ${device}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.1.1451.4 Mobile Safari/537.36`,
         ]
 
-        return UAS[this.randint(0, UAS.length)] + ` miHoYoBBS/${appVer}`
-    }
-
-    public static randRef(act_id: string) {
-        let Refs = [
-            'https://app.mihoyo.com',
-            `https://webstatic.mihoyo.com/bbs/event/signin-ys/index.html?bbs_auth_required=true&act_id=${act_id}&utm_source=bbs&utm_medium=mys&utm_campaign=icon`
-        ]
-
-        return Refs[this.randint(0, Refs.length)]
+        return UAS[this.randint(0, UAS.length)]
     }
 }
