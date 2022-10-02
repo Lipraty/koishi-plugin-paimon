98e5e3e56a81d89cf115841157e45ed46d6537d0
Lipraty <sis1943@outlook.com>
2022-10-03 03:38:16 +0800

    add(genshin): `dailyNote`

--- src/genshin/index.ts
+++ src/genshin/index.ts
@@ -7,12 +7,39 @@ export namespace Genshin {
         /**
          * 米游社签到
          */
-        public async bbsSign() {
-            return await this.fetchAPI('bbsSign', this.hoyoKit.signHeader(this.cookie), {
+        public async bbsSign(): Promise<SignInfo> {
+            const reqData = await this.fetchAPI('bbsSign', this.hoyoKit.signHeader(this.cookie), {
                 act_id: this.hoyoKit.act_id,
                 region: this.serverType,
                 uid: this.uid
             })
+            if (reqData.retcode !== 0 || !reqData.data) {
+                throw {
+                    code: reqData.retcode,
+                    message: reqData.message,
+                }
+            }
+
+            return reqData.data
+        }
+        /**
+         * 实时便笺
+         */
+        public async dailyNote(): Promise<DailyNote> {
+            const params = {
+                role_id: this.uid,
+                server: this.serverType
+            }
+
+            const reqData = await this.fetchAPI('dailyNote', this.hoyoKit.headers(this.cookie, params), params)
+            if (reqData.retcode !== 0 || !reqData.data) {
+                throw {
+                    code: reqData.retcode,
+                    message: reqData.message,
+                }
+            }
+
+            return reqData.data
         }
     }
     export class Gacha extends GachaCore {
