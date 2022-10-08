857d3df00a8aee9eeacbe3ff2d63001c8de2364d
Lipraty <sis1943@outlook.com>
2022-10-09 04:15:02 +0800

    chore(command): delete `UID` value check

--- src/command/index.ts
+++ src/command/index.ts
@@ -3,16 +3,3 @@ import user from "./user.option";
 import memo from "./memo.option";
 
 export const optModules = [user, memo]
-
-declare module 'koishi' {
-    namespace Argv {
-        interface Domain {
-            UID: string
-        }
-    }
-}
-
-Argv.createDomain('UID', source => {
-    if (isNaN(parseInt(source))) throw new Error('这不是一个正确的UID')
-    return source
-})
