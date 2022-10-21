9bb078ea7d3702ffc0b94bb8a2e7ddaa7b3ded76
Lipraty <sis1943@outlook.com>
2022-10-22 02:06:04 +0800

    refa: database structure

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,7 +12,6 @@ declare module 'koishi' {
 export interface PaimonUser {
     uuid: string
     user: string
-    uid: UID[]
     active_uid: UID
     characet_id: number[]
 }
@@ -20,13 +19,20 @@ export interface PaimonUser {
 interface PaimonUserAll {
     uuid: string
     user: string
-    uid: PaimonUid[]
-    active_uid: UID
-    characet_id: number[] | PaimonCharacter[]
+    uid: PaimonUserUID[]
+    active_uid?: UID
+    characet_id: number[]
+}
+
+interface PaimonUserUID {
+    uid: UID
+    dsalt?: string
+    cookie?: string
 }
 
 export interface PaimonUid {
     uid: UID
+    uuid: string
     dsalt: string
     cookie: string
 }
@@ -51,7 +57,7 @@ export class Database {
     private _created: boolean = false
     private context: Context
     private static context: Context
-    private static userData: PaimonUser
+    private static userData: PaimonUserAll
     private static userMaster: boolean = false
 
     public constructor(ctx: Context) {
@@ -65,13 +71,11 @@ export class Database {
             this.context.model.extend('paimon_user', {
                 uuid: 'string',
                 user: 'string',
-                uid: 'list',
                 active_uid: 'string',
                 characet_id: 'list'
             }, {
                 primary: 'uuid',
                 foreign: {
-                    uid: ['paimon_uid', 'uid'],
                     characet_id: ['paimon_character', 'cuid']
                 },
                 unique: ['uuid', 'user']
@@ -79,11 +83,14 @@ export class Database {
 
             this.context.model.extend('paimon_uid', {
                 uid: 'string',
+                uuid: 'text',
                 dsalt: 'text',
                 cookie: 'text',
             }, {
                 primary: 'uid',
-                foreign: {},
+                foreign: {
+                    uuid: ['paimon_user', 'uuid']
+                },
                 unique: ['uid', 'dsalt', 'cookie']
             })
 
@@ -125,7 +132,6 @@ export class Database {
             return await this.context.database.create('paimon_user', {
                 uuid,
                 user,
-                uid: [],
                 characet_id: []
             })
         } else {
@@ -141,29 +147,52 @@ export class Database {
     public static user(user: string, master: boolean = false) {
         //先创建用户
         this.createUser(user).then(usr => {
-            //缓存用户数据
-            this.userData = usr
+            //获取该用户绑定的uid
+            this.context.database.get('paimon_uid', { uuid: usr.uuid }).then((uid: PaimonUid[]) => {
+                this.userData = {
+                    uuid: usr.uuid,
+                    user: usr.user,
+                    uid: uid.map(V => ({ uid: V.uid, cookie: V.cookie, dsalt: V.dsalt })),
+                    active_uid: usr.active_uid,
+                    characet_id: usr.characet_id
+                }
+            })
         })
+
         //更新用户权限情况
         this.userMaster = master
         return this
     }
 
-    public static get uuid(){
+    //#region user datas
+    public static get uuid() {
         return this.userData.uuid
     }
 
-    public static get uid(){
+    public static get uid() {
         return this.userData.uid
     }
 
-    public static get activeUID(){
+    // public static set uid(uid) {
+    //     this.userData.uid = uid
+    // }
+
+    public static get activeUID() {
         return this.userData.active_uid
     }
 
-    public static get characterID(){
+    public static set activeUID(activeUID) {
+        this.userData.active_uid = activeUID
+    }
+
+    public static get characterID() {
         return this.userData.characet_id
     }
+
+    // public static set characterID(characterID) {
+    //     this.userData.characet_id = characterID
+    // }
+    //#endregion
 
     /**
      * 将用户数据操作同步至数据库
@@ -171,8 +200,16 @@ export class Database {
      * @database
      */
     public static async push() {
-        //以UUID为基准进行更新
-        await this.context.database.upsert('paimon_user', [this.userData], 'uuid')
+        //更新能修改的用户数据
+        await this.context.database.upsert('paimon_user', [{
+            active_uid: this.userData.active_uid,
+            characet_id: this.userData.characet_id
+        }], 'uuid')
+        //同步uid
+        await this.context.database.upsert('paimon_uid',
+            //将uuid合并至用户uid表
+            this.userData.uid.map(uid => Object.assign(uid, { uuid: this.userData.uuid })),
+            'uuid')
     }
 
     /**
@@ -185,39 +222,11 @@ export class Database {
     }
 
     /**
-     * 查询该用户下所有uid
-     * - master用户拥有所有uid
-     * @database
-     */
-    public static async findUID(): Promise<UID[]> {
-        if (this.userMaster) {
-            const uidT = await this.context.database.get('paimon_uid', {})
-            //format
-            return uidT.map(val => {
-                return val.uid
-            })
-        } else {
-            return this.userData.uid
-        }
-    }
-
-    /**
-     * 查询该用户下完整uid信息
-     * - master用户拥有所有uid信息
-     * @database
-     */
-    public static async fullUID(): Promise<PaimonUid[]> {
-        return await this.context.database.get('paimon_uid',
-            this.userMaster ? {} : { uid: this.uid }
-        )
-    }
-
-    /**
      * 查询用户角色信息
      * - master用户拥有所有角色信息
      * @database
      */
-    public static async characet(cuid?: number[]): Promise<PaimonCharacter[]> {
+    public static async characters(cuid?: number[]): Promise<PaimonCharacter[]> {
         if (cuid) {
             //未指定，将用户绑定的所有角色ID加上
             cuid = this.userData.characet_id
@@ -236,21 +245,31 @@ export class Database {
      * @param allCharacet 是否携带完整角色数据（由于数据过多，为减轻负担默认关闭）
      */
     public static async all(allCharacet: boolean = false): Promise<PaimonUserAll> {
-        return {
-            uuid: this.userData.uuid,
-            user: this.userData.user,
-            uid: await this.fullUID(),
-            active_uid: this.userData.active_uid,
-            characet_id: allCharacet ? await this.characet() : this.userData.characet_id
-        }
+        let d = this.userData
+        if (allCharacet)
+            d['characters'] = await this.characters()
+        return d
+    }
+
+    public static uidObject(): Record<UID, PaimonUserUID> {
+        return Object.fromEntries(this.userData.uid.map(uidObj => {
+            return [uidObj.uid, uidObj]
+        }))
     }
 
     private static async _findUIDTable(uid: UID, tableKey: Keys<PaimonUid>) {
-        const uidT: PaimonUid[] = await this.context.database.get('paimon_uid', { uid })
-        if (this.uid.includes(uid) && uidT.length > 0) {
+        if (this.userMaster) {
+            //高权限直接查询uid表
+            const uidT = await this.context.database.get('paimon_uid', { uid })
             return uidT[0][tableKey]
         } else {
-            throw undefined
+            //用户拥有的uid组
+            const userUIDArr = this.userData.uid.map(V => V.uid)
+            if (userUIDArr.includes(uid) && userUIDArr.length > 0) {
+                return this.uidObject()[uid][tableKey]
+            } else {
+                throw undefined
+            }
         }
     }
     /**
@@ -265,39 +284,45 @@ export class Database {
     public static deviceSlat = async (uid: UID): Promise<string> => this._findUIDTable(uid, 'dsalt') as Promise<string>
 
     /**
-     * 设置默认uid
-     * @param uid 
-     */
-    public static setActive(uid: UID) {
-        this.userData.active_uid = uid
-    }
-
-    /**
      * 创建uid数据
      * @param uid 
      */
     private static async createUID(uid: UID, cookie?: string) {
         const uidT = await this.context.database.get('paimon_uid', { uid })
+        //只有该uid不存在时创建
         if (uidT.length === 0) {
             const dsalt = UUID.randomUUID().unsign()
-            return await this.context.database.create('paimon_uid', { uid, cookie, dsalt })
+            return await this.context.database.create('paimon_uid', { uuid: this.userData.uuid, uid, cookie, dsalt })
         } else {
             return uidT[0]
         }
     }
 
     /**
-     * 绑定这个uid
+     * 绑定一个uid
      * @param uid 
-     * @returns {Promise<UID[]>} 该用户绑定的所有uid
+     * @returns {Promise<PaimonUserUID[]>} 该用户绑定的所有uid
      */
-    public static async setUID(uid: UID): Promise<UID[]> {
-        await this.createUID(uid)
+    public static async setUID(uid: UID): Promise<PaimonUserUID[]> {
         //如果未绑定过，则设置为默认uid
         if (this.userData.uid.length === 0)
-            this.setActive(uid)
+            this.userData.active_uid = uid
+        //创建uid数据
+        await this.createUID(uid)
         //将uid加入绑定列表
-        this.userData.uid.push(uid)
+        this.userData.uid.push({ uid })
+        return this.userData.uid
+    }
+
+    /**
+     * 移除这个uid，并返回新的uid组
+     * @param uid 
+     */
+    public static async removeUID(uid: UID) {
+        //在数据库中移除该uid
+        await this.context.database.remove('paimon_uid', { uid })
+        //设置为新的uid表数据
+        this.userData.uid = await this.context.database.get('paimon_uid', { uuid: this.userData.uuid })
         return this.userData.uid
     }
 
@@ -307,8 +332,8 @@ export class Database {
      * @param uid 指定cookie绑定到的uid
      */
     public static async setCookie(cookie: string, uid: UID = this.userData.active_uid) {
-        if (!this.userData.uid.includes(uid) && !this.userMaster) {
-            throw 'forbidden'
+        if (!Object.keys(this.uidObject()).includes(uid as string) && !this.userMaster) {
+            throw undefined
         }
         return await this.context.database.set('paimon_uid', { uid }, { cookie })
     }
