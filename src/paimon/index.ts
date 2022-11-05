4ce227cac782816d97365381c08aa305040706d7
Lipraty <sis1943@outlook.com>
2022-11-05 18:00:57 +0800

    refa: absctact hack

---
+++ src/paimon/index.ts
@@ -0,0 +1,3 @@
+export * from "./configs"
+export * from "./database"
+export * from "./session"
--- src/paimon/session.ts
+++ src/paimon/session.ts
@@ -1,8 +1,31 @@
-import { User, Channel, Session } from "koishi";
+import { User, Channel, Session, Context, observe } from "koishi";
+import { Bot } from "@satorijs/satori"
 import { PaimonUid } from "./database";
+import { extend } from "../utils/Misc.util";
 
-declare module "koishi" {}
-
-export class PaimonSession<U extends User.Field, G extends Channel.Field, P extends PaimonUid.Field> extends Session<U, G> {
-    paimon: PaimonUid.Observed<P>
+declare module "koishi" {
+    interface Session {
+        $uids: PaimonUid.Observed<PaimonUid.Field>
+        getUid<K extends PaimonUid.Field = never>(uuis?: string, fields?: K[]): Promise<PaimonUid>
+        observeUid<T extends PaimonUid.Field = never>(fields?: Iterable<T>): Promise<PaimonUid.Observed<T>>
+        collect<T extends "user" | "channel" | "paimon_uid" | "paimon_character">(key: T, argv: Argv<never, never, any[], {}>, fields?: Set<keyof Tables[T]>): Set<keyof Tables[T]>
+    }
 }
+
+extend(Session.prototype as Session, {
+
+    async observeUid(fields = []) {
+
+        return this.$uids
+    },
+
+    async getUid(uuid, fields) {
+        return {
+            uuid,
+            uid: '1',
+            cookie: '',
+            dsalt: '',
+            freeze: false
+        }
+    }
+})
