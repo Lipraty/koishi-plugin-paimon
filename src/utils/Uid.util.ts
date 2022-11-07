108296229966988019b82d2e9e2ae2ecd51ebb00
Lipraty <sis1943@outlook.com>
2022-11-07 16:42:09 +0800

    chore: tagging error

--- src/utils/Uid.util.ts
+++ src/utils/Uid.util.ts
@@ -27,7 +27,7 @@ export function onlyUid(uids: PaimonUid[]): UID[] {
 export function uidStringifyMap(active: UID, uids: PaimonUid[], fromatFn: (value: UID, index: number) => string): string[] {
     const activeIndex = onlyUid(uids).indexOf(active)
     if (activeIndex === -1)
-        throw TypeError('Type `active` should exist.')
+        throw TypeError('[paimon]Type `active` should exist.')
     //active uid to frist
     uids.unshift(uids.splice(activeIndex, 1)[0])
     return uids.map((val, i) => fromatFn(val.uid, i))
